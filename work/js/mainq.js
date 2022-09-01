3d17fec57771976f9d416b963684a5dd43addaf5
Ilya <112631624+IlyaKalachin@users.noreply.github.com>
2022-09-01 22:07:02 +0300

    ilya

---
+++ work/js/mainq.js
@@ -0,0 +1,15 @@
+$(function () {
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 & $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  })
+
+$('.burger, .overlay, .header__top a').on('click', function (e) {
+  e.preventDefault()
+  $('.header__top').toggleClass('header__top--open')
+  $('.overlay').toggleClass('overlay--show')
+})
+})
